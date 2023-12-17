0ba5e6cf245a1d7c44d2a3820f3492ee72c8bd04
Khushi-H <154000684+Khushi-H@users.noreply.github.com>
2023-12-17 11:04:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import colors from "colors"
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-
 import cors from "cors";
+import path from "path";
+import {fileURLToPath} from 'url'
 // configure env
 dotenv.config();
 
 // dataabase config
 connectDB();
+
+// esmodule fix
+const __filename = fileURLToPath(import.meta.url)
 
 // rest object
 const app = express();
@@ -20,6 +25,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/public")));
 
 // routes
 app.use("/api/v1/auth", authRoute);
@@ -27,8 +33,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 // rest api
-app.get("/", (req, res) => {
-  res.send("welcome hello");
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 // port
